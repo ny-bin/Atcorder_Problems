27f6e700a61f5da843636060e4d77d0f93c8fab8
ny-bin <ny.bin1210@gmail.com>
2021-07-18 13:58:33 +0900

    ABC210復習

--- ABC/210/C.py
+++ ABC/210/C.py
@@ -1,9 +1,6 @@
-import collections
-
 N, K = map(int, input().split())
 C = list(map(int, input().split()))
 
-ans = 0
 # for n in range(N):
 #     sL = C[n:n + K]
 #     setNums = list(set(sL))
@@ -14,13 +11,41 @@ ans = 0
 # print(ans)
 
 
-sL = C[0:K]
+# sL = C[0:K]
+# ans = 0
+# setNums = list(set(sL))
+# for n in range(K, N):
+#     ans = max(ans, len(setNums))
+#     num = C[n]
+#     if num in setNums:
+#         break
+#     else:
+#         setNums.append()
+
+dic = {}
+first = []
 ans = 0
-setNums = list(set(sL))
-for n in range(K, N):
-    ans = max(ans, len(setNums))
-    num = C[n]
-    if num in setNums:
-        break
+
+for n in range(N):
+    target = C[n]
+    if n < K:
+        if target in dic:
+            dic[target] += 1
+        else:
+            dic[target] = 1
+        ans = max(len(dic), ans)
+        continue
     else:
-        setNums.append()
+        if target in dic:
+            dic[target] += 1
+        else:
+            dic[target] = 1
+
+    del_num = C[n - K]
+    if del_num in dic:
+        dic[del_num] -= 1
+    if dic[del_num] == 0:
+        del dic[del_num]
+    ans = max(len(dic), ans)
+
+print(ans)
