352d1fe38c66ffb4c7fc61dcd78ebbee92d5893d
ny-bin <ny.bin1210@gmail.com>
2021-10-19 08:55:10 +0900

    10/19 arc 99 c 2

--- ARC/99/C.py
+++ ARC/99/C.py
@@ -1,5 +1,11 @@
+import math
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
-ans = (N - 1 + K - 2) // (K - 1)
+ans = math.ceil((N - 1) / (K - 1))
 print(ans)
+# ans = (N - 1 + K - 2) // (K - 1)
+# print(ans)
+
+# △
+# 10/19 解き直し○
