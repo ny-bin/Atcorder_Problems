2cb09c0e662977a744e487875843dad6e4c56428
bin <ny.bin1210@gmail.com>
2021-10-26 09:21:38 +0900

    10/26 みんなのプロコン2019 c

--- みんなのプロコン2019/C.py
+++ みんなのプロコン2019/C.py
@@ -2,15 +2,13 @@ K, A, B = map(int, input().split())
 
 bisket = 1
 money = 0
-if B <= A + 1:
-    print(K + 1)
-elif K <= A:
+if B - A <= 2:
     print(K + 1)
 else:
-    while True:
-        K -= A
-        money += 1
-        if K - money < A:
-            break
-
-    print((K - money) + (money * B))
+    # 交換可能回数
+    count = max(0, (K - A + 1) // 2)
+    ans = A + ((B - A) * count)
+    if (K - A + 1) % 2 == 1:
+        ans += 1
+    print(ans)
+# ×
